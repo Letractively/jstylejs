aa7ed031000c27626de838e831d165e81e8aff23
creativor@gmail.com <creativor@gmail.com@334f77d0-816f-11de-9ebc-19e80b7c425a>
2011-01-30 07:43:34 +0000

    Add library.

---
+++ TestRPC/src/common/DesignNotes.java
@@ -0,0 +1,20 @@
+package common;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Retention(RetentionPolicy.SOURCE)
+@Target(ElementType.TYPE)
+public @interface DesignNotes {
+
+	Class[] requires() default {};
+
+	String responsibility() default "";
+
+	Class[] usedBy() default {};
+
+}
---
+++ TestRPC/test/common/RpcPacketTest.java
@@ -0,0 +1,79 @@
+package common;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class RpcPacketTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testRpcPacketLongLongShort() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDataLength() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetChecksum() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRpcPacketLong() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetChecksum() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetData() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCallId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetData() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWrite() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
