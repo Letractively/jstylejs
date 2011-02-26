df26b0d9c909f20a6c510df7556031d6aa781f10
creativor <creativor@334f77d0-816f-11de-9ebc-19e80b7c425a>
2011-02-26 17:23:45 +0000

    Submit codes.

--- Rayson/src/org/rayson/api/RpcException.java
+++ Rayson/src/org/rayson/api/RpcException.java
@@ -1,9 +1,7 @@
 package org.rayson.api;
 
-
 public class RpcException extends Exception {
 	private static final long serialVersionUID = 1L;
-	private Throwable cause;
 
 	public RpcException(Throwable cause) {
 		super(cause);
