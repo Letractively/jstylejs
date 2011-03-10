c9b71fcf06a2dc3d805a6fde5f7298fd4662f6b0
creativor@gmail.com <creativor@gmail.com@334f77d0-816f-11de-9ebc-19e80b7c425a>
2011-03-10 06:29:24 +0000

    Reflector.

--- Rayson-apt/src/org/rayson/apt/RpcServiceProcessor.java
+++ Rayson-apt/src/org/rayson/apt/RpcServiceProcessor.java
@@ -6,39 +6,57 @@ import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
 
+@SupportedSourceVersion(SourceVersion.RELEASE_6)
 public class RpcServiceProcessor extends AbstractProcessor {
 	public RpcServiceProcessor() {
+
 	}
 
-	private static final String PROTOCOLS__ANNOTATION_NAME = "org.rayson.annotation.RpcProtocols";
+	private static final String PROTOCOLS_ANNOTATION_NAME = "org.rayson.annotation.Protocols";
 
 	private static final Set<String> SUPPORTED_ANNOTATION_TYPES;
 	static {
 		SUPPORTED_ANNOTATION_TYPES = new HashSet<String>();
-		SUPPORTED_ANNOTATION_TYPES.add(PROTOCOLS__ANNOTATION_NAME);
+		SUPPORTED_ANNOTATION_TYPES.add(PROTOCOLS_ANNOTATION_NAME);
 	}
 
 	@Override
 	public boolean process(Set<? extends TypeElement> annotations,
 			RoundEnvironment roundEnv) {
 		try {
-			Set<? extends TypeElement> rpcServiceTypes = (Set<? extends TypeElement>) roundEnv
-					.getElementsAnnotatedWith(annotations.iterator().next());
-			for (TypeElement rpcServiceType : rpcServiceTypes) {
-				rpcServiceType.accept(
-						new ServiceTypeVisitor(
-								this.processingEnv.getMessager(),
-								(Class[]) rpcServiceType.getAnnotationMirrors()
-										.get(0).getElementValues().values()
-										.iterator().next().getValue()), null);
+			for (Element typeElement : getElementsAnnotationedWithService(
+					annotations, roundEnv)) {
+				System.out.println(typeElement);
 			}
-			return true;
 		} catch (Throwable e) {
 			e.printStackTrace();
 			return false;
 		}
+		return false;
+	}
+
+	private Set<? extends Element> getElementsAnnotationedWithService(
+			Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+		TypeElement typeElement = getServiceAnnotationTypeElement(annotations);
+		if (typeElement == null)
+			return Collections.EMPTY_SET;
+		else
+			return roundEnv.getElementsAnnotatedWith(typeElement);
+	}
+
+	private TypeElement getServiceAnnotationTypeElement(
+			Set<? extends TypeElement> annotations) {
+		for (TypeElement typeElement : annotations) {
+			if (typeElement.getQualifiedName().contentEquals(
+					PROTOCOLS_ANNOTATION_NAME))
+				return typeElement;
+		}
+		return null;
 	}
 
 	@Override
