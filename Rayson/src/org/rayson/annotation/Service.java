784e4959b80b4a218bcff9dfe55d9bb4eb522a6a
creativor@gmail.com <creativor@gmail.com@334f77d0-816f-11de-9ebc-19e80b7c425a>
2011-03-10 03:46:55 +0000

    Reflector.

--- Rayson/src/org/rayson/annotation/Service.java
+++ Rayson/src/org/rayson/annotation/Service.java
@@ -1,13 +1,14 @@
 package org.rayson.annotation;
 
-import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
-@Documented
+@Target(ElementType.TYPE)
 public @interface Service {
 
 }
