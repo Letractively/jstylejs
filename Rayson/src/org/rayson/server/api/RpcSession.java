8b2e28c660c8ddf2f9c6824cc994cafa0df5df1b
creativor@gmail.com <creativor@gmail.com@334f77d0-816f-11de-9ebc-19e80b7c425a>
2011-03-14 08:40:32 +0000

    Modified.

--- Rayson/src/org/rayson/server/api/RpcSession.java
+++
@@ -1,9 +0,0 @@
-package org.rayson.server.api;
-
-import java.net.SocketAddress;
-
-import org.rayson.api.Session;
-
-public interface RpcSession extends Session {
-	public SocketAddress getRemoteAddr();
-}
