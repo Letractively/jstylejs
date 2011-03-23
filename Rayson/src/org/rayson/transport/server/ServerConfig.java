5ed6e13eb949cab81109f72db86e61fc38da0566
creativor@gmail.com <creativor@gmail.com@334f77d0-816f-11de-9ebc-19e80b7c425a>
2011-03-23 02:59:46 +0000

    Add Config and ServerConfig annotations.

--- Rayson/src/org/rayson/transport/server/ServerConfig.java
+++ Rayson/src/org/rayson/transport/server/ServerConfig.java
@@ -1,11 +1,13 @@
 package org.rayson.transport.server;
 
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 import org.rayson.annotation.Config;
 
 @Config
+@Inherited
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ServerConfig {
 	public short portNumber() default 4465;
