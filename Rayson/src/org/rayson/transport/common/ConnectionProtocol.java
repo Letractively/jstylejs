81a955485bd006fe8e45d89bc77430a7cfba018e
creativor@gmail.com <creativor@gmail.com@334f77d0-816f-11de-9ebc-19e80b7c425a>
2011-03-24 01:37:24 +0000

    Add transfer protocol comments.

--- Rayson/src/org/rayson/transport/common/ConnectionProtocol.java
+++ Rayson/src/org/rayson/transport/common/ConnectionProtocol.java
@@ -1,5 +1,7 @@
 package org.rayson.transport.common;
 
+import org.rayson.transport.stream.TransferResponse;
+
 /**
  * Define the protocol of the connection between client and server. <br>
  * <h4>Connection header format:</h4>
@@ -43,7 +45,25 @@ package org.rayson.transport.common;
  * </table>
  * <br/>
  * See {@link RequestType} about packet request type. <br/>
+ * <h4>Transfer request header format:</h4>
+ * <table border="1">
+ * <tr>
+ * <td>transfer code(2)</td>
+ * <td>data length(2)</td>
+ * <td>argument data</td>
+ * </tr>
+ * </table>
  * 
+ * <h4>Transfer response header format:</h4>
+ * <table border="1">
+ * <tr>
+ * <td>response code(1)</td>
+ * <td>reserved(127)</td>
+ * </tr>
+ * </table>
+ * <br/>
+ * 
+ * See {@link TransferResponse} about transfer response code. <br/>
  * <br>
  * Please notes, all size unit is <b>Byte</b>.
  * 
@@ -54,7 +74,10 @@ public final class ConnectionProtocol {
 	public static final int MAX_PENDING_PACKETS = 1000;
 	public static final int PACKET_HEADER_LENGTH = 5; // type+data
 	// length+checksum
-	public static final int RESPONSE_LENGTH = 128;// connection code_reserved
+	public static final int RESPONSE_LENGTH = 128;// connection response
+													// state+reserved
+	public static final int TRANSFER_RESPONSE_LENGTH = 128;// transfer response
+															// code+reserved
 	public static final int TIME_OUT_INTERVAL = 600 * 1000;
 
 }
