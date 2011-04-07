fd456735efe00587f0d609afba76b50ee87e977f
creativor@gmail.com <creativor@gmail.com@334f77d0-816f-11de-9ebc-19e80b7c425a>
2011-04-07 08:15:50 +0000

    Add demo

--- Viva/bin/start.sh
+++ Viva/bin/start.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-
+cd ..
+CURRENT_DIR=$PWD
+CLASS_PATH=$CURRENT_DIR/build/
+for f in $CURRENT_DIR/lib/*.jar;do
+CLASS_PATH=$CLASS_PATH:$f;
+done
+echo $CLASS_PATH
+java -cp $CLASS_PATH  org.creativor.viva.Viva $*
