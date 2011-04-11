2e63f83164e2697b7a8ec8aa39348b5e7d6d386d
creativor@gmail.com <creativor@gmail.com@334f77d0-816f-11de-9ebc-19e80b7c425a>
2011-04-11 08:00:33 +0000

    Modified.

---
+++ Viva/bin/demo/multi-start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+CURRENT_DIR=$PWD
+cd ..
+
+for (( i=6660; i<6670; i++))
+do
+	sh ./start.sh $i &
+	sleep 5
+done
+
+cd $CURRENT_DIR
+
---
+++ Viva/bin/shell.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+CURRENT_DIR=$PWD
+cd ..
+PATENT_PATH=$PWD
+CLASS_PATH=$PATENT_PATH/build/
+for f in $PATENT_PATH/lib/*.jar;do
+CLASS_PATH=$CLASS_PATH:$f;
+done
+cd $CURRENT_DIR
+java -cp $CLASS_PATH  org.creativor.viva.shell.Shell $*
